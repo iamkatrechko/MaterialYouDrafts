1b1191497f99f67537c5ba5bf27211177dbb5c23
iamkatrechko <iamkatrechko@yandex.ru>
2021-11-08 17:38:32 +0300

    Initial commit

---
+++ app/src/main/java/com/iamkatrechko/md3drafts/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.iamkatrechko.md3drafts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
