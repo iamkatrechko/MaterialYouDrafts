588cafc68bd29ebce89d6f6b42a6896c7a6e15a1
iamkatrechko <iamkatrechko@yandex.ru>
2021-11-09 08:52:41 +0300

    Добавление App класса

---
+++ app/src/main/java/com/iamkatrechko/md3drafts/App.kt
@@ -0,0 +1,15 @@
+package com.iamkatrechko.md3drafts
+
+import android.app.Application
+
+/**
+ *
+ * @author iamkatrechko
+ *         Date: 09.11.2021
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
